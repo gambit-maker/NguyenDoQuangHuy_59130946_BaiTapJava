b399ddf956c7ab358f5676b2efd2c93b1b53a201
HP <HP@DESKTOP-AF6UDPR>
2020-04-11 22:18:18 +0700

    V1.4

--- src/BaiTap1/BaiTap1Main.java
+++ src/BaiTap1/BaiTap1Main.java
@@ -22,7 +22,7 @@ public class BaiTap1Main {
         System.out.println(nv1.getThongTin());
         System.out.println("Nhan vien 2");
         System.out.println(nv2.getThongTin());
-        System.out.println("Hello World");
+        System.out.println("Hello World Testing");
     }
     
 }
